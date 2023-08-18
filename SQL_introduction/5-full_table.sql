729385e1864aaf42c9e142783b8e1521f6eb5156
Eugene Ncube <philosncube@gmail.com>
2023-08-18 20:09:03 +0200

    hotfix

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,5 +1,3 @@
 -- SQL script that prints out description of table
 
-SELECT TABLE_NAME, CREATE_TABLE
-FROM INFORMATION_SCHEMA.TABLES
-WHERE TABLE_SCHEMA = 'hbtn_0c_0' AND TABLE_NAME = 'first_table';
+SHOW id, name FROM first_table;
