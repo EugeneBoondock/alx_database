236ab23bb1e42f4dc1a369144038d6cfbff4f316
Eugene Ncube <philosncube@gmail.com>
2023-08-18 20:00:24 +0200

    minor fix

--- SQL_introduction/6-list_values.sql
+++ SQL_introduction/6-list_values.sql
@@ -1,12 +1,3 @@
 -- SQL script to retrieve rows from table
 
-CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
-
-USE hbtn_0c_0;
-
-CREATE TABLE IF NOT EXISTS first_table (
-    id INT PRIMARY KEY,
-    name VARCHAR(256)
-);
-
-SELECT * FROM first_table;
+SELECT id, name FROM first_table
