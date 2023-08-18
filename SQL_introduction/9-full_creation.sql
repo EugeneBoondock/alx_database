a08f2d870598aa84ea586851b5e6d13b3d5bcc32
Eugene Ncube <philosncube@gmail.com>
2023-08-18 23:54:01 +0200

    please work

---
+++ SQL_introduction/9-full_creation.sql
@@ -0,0 +1,10 @@
+-- SQL script that creates a table
+
+CREATE TABLE IF NOT EXISTS hbtn_0c_0.second_table (
+	id INT PRIMARY KEY,
+	name VARCHAR(256)
+	score INT
+);
+
+INSERT INTO second_table (id, name, score)
+VALUES (1, 'John', 10), (2, 'Alex', 3), (3, 'Bob', 14), (4, 'George', 8);
