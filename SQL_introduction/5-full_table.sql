a4a12dee9f26ef1e2a173fa4d538405451443b4a
Eugene Ncube <philosncube@gmail.com>
2023-08-18 03:23:19 +0200

    2 fixes

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,3 +1,3 @@
 -- SQL script that prints out description of table
 
-DESC hbtn_0c_0.first_table;
+SHOW COLUMNS FROM hbtn_0c_0.first_table;
---
+++ SQL_introduction/6-list_values.sql
@@ -0,0 +1,3 @@
+-- SQL script shows rows from table
+
+SHOW ROWS FROM hbtn_0c_0.first_table;
