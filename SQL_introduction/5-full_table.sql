4206b0f2cd59bad0ab991e69fe0222b3d9aeb4fa
Eugene Ncube <philosncube@gmail.com>
2023-08-18 03:44:30 +0200

    multiple fixes

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,11 +1,12 @@
 -- SQL script that prints out description of table
 
-CREATE DATABASE hbtn_0c_0
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
 
-CREATE TABLE IF NOT EXISTS first_table (
+USE hbtn_0c_0;
+
+CREATE TABLE IF NOT EXISTS first_table(
     id INT PRIMARY KEY,
     name VARCHAR(256)
 );
 
-USE hbtn_0c_0;
 DESC first_table;
--- SQL_introduction/6-list_values.sql
+++ SQL_introduction/6-list_values.sql
@@ -1,11 +1,12 @@
 -- SQL script to retrieve rows from table
 
-CREATE DATABASE hbtn_0c_0;
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
+
+USE hbtn_0c_0;
 
 CREATE TABLE IF NOT EXISTS first_table (
     id INT PRIMARY KEY,
     name VARCHAR(256)
 );
 
-USE hbtn_0c_0;
 SELECT * FROM first_table;
