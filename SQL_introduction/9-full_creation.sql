08f3c4d7d9b39e2f52cb655cfcbd2f6e3b800c67
Eugene Ncube <philosncube@gmail.com>
2023-08-18 23:55:18 +0200

    hotfix

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,6 +1,6 @@
 -- SQL script that creates a table
 
-CREATE TABLE IF NOT EXISTS hbtn_0c_0.second_table (
+CREATE TABLE IF NOT EXISTS second_table (
 	id INT PRIMARY KEY,
 	name VARCHAR(256)
 	score INT
