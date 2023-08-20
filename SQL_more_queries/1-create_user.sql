2308a3cfeaea50450b454c67b18038de0894f21a
Eugene Ncube <philosncube@gmail.com>
2023-08-20 22:14:01 +0200

    fix

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,5 +1,5 @@
 -- SQL script to create User
 
 CREATE USER 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES ON TO *.* 'user_0d_1'@'localhost';
+GRANT ALL PRIVILEGES ON TO 'user_0d_1'@'localhost';
 FLUSH PRIVILEGES;
