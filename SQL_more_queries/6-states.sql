0c5f23170303dd01647a17c53c86b1bbf9176984
Eugene Ncube <philosncube@gmail.com>
2023-08-20 23:25:14 +0200

    database auto

---
+++ SQL_more_queries/6-states.sql
@@ -0,0 +1,8 @@
+-- SQL script that creates database
+
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS states (
+    id INT UNIQUE AUTO PRIMARY KEY,
+    name VARCHAR(256) NOT NULL
+);
